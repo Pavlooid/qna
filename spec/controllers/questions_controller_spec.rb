c406af4d3c0cbe4b45d4cfc682db316bd10b19e1
Pavlooid <pavlooo987@gmail.com>
2023-11-29 16:45:19 +0300

    Screencast part 2

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,19 +1,123 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
-    let!(:questions) { create_list(:question, 3) }
+    let(:questions) { create_list(:question, 3) }
+
+    before { get :index }
 
     it 'show all questions' do
-
-      get :index
-
       expect(assigns(:questions)).to match_array(questions)
     end
+
     it 'render index view' do
-      get :index
       expect(response).to render_template :index
     end
   end
 
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'questions to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question } }
+
+    it 'questions to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid' do
+      it 'save question in db' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response). to redirect_to assigns(:question)
+      end
+    end
+
+    context 'invalid' do
+      it 'no save' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+      it 're-render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'valid' do
+      it 'question to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+      it 'change @question' do
+        patch :update, params: { id: question, question: { title: 'da', body: 'da' } }
+        question.reload
+
+        expect(question.title).to eq 'da'
+        expect(question.body).to eq 'da'
+      end
+      it 'redirect to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(response). to redirect_to question
+      end
+    end
+
+    context 'not valid' do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+      it 'no change' do
+        question.reload
+
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'
+      end
+      it 're-render edir view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+    
+    it 'delete question' do 
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to question_path
+    end
+  end
 end
