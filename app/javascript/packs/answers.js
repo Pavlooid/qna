aa2f1577ed4c5b2784f20000c5b6361007277063
Pavlooid <pavlooo987@gmail.com>
2023-12-13 15:38:53 +0300

    Fixed js

--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $('.answers').on('click', '.edit-answer-link', function(e) {
+    console.log(123)
     e.preventDefault();
     $(this).hide();
     let answerId = $(this).data('answerId');
     $('form#edit-answer-' + answerId).removeClass('hidden');
-    console.log('12345');
   })
 
 });
