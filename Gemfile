66cee68e231029493254515b884bd66cb4221146
Pavlooid <pavlooo987@gmail.com>
2024-01-05 16:13:25 +0300

    Fixed bucket issue and add author check

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise'
 gem 'jquery-rails'
 gem 'bootstrap'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,7 +3,9 @@ class FilesController < ApplicationController
 
   def purge
     file = ActiveStorage::Attachment.find(params[:id])
-    file.purge
-    redirect_back fallback_location: root_path, alert: "#{file.filename.to_s} deleted succesfully!"
+    if file.record.author == current_user
+      file.purge
+      redirect_back fallback_location: root_path, alert: "#{file.filename.to_s} deleted succesfully!"
+    end
   end
 end
