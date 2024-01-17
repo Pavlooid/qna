7e0a1af91f6352b20745007d4740aab5c2c0aef4
Pavlooid <pavlooo987@gmail.com>
2024-01-17 13:30:04 +0300

    Add additional info

--- spec/features/question/add_comment_spec.rb
+++ spec/features/question/add_comment_spec.rb
@@ -7,16 +7,32 @@ feature 'User can add comments to question', %q{
 } do
 
   given(:user) { create(:user) }
+  given!(:user2) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'Authorized user adds comment to answer', js: true do
-    sign_in(user)
-    visit question_path(question)
+  context 'multiple sessions' do
+    scenario 'Authorized user adds comment to question that can see any other user', js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
 
-    fill_in 'Body', with: "Ruby", match: :first
-    click_on 'Add comment'
+      Capybara.using_session('user2') do
+        sign_in(user2)
+        visit question_path(question)
+      end
 
-    expect(page).to have_content "Ruby"
+      Capybara.using_session('user') do
+        fill_in 'Body', with: "Ruby", match: :first
+        click_on 'Add comment'
+
+        expect(page).to have_content "Ruby"
+      end
+
+      Capybara.using_session('user2') do
+        expect(page).to have_content "Ruby"
+      end
+    end
   end
 
   scenario "Authorized user can't add comment", js: true do
