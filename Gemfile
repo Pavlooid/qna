0b1d5573439676b57b94e42080ced3d8eabe798a
Pavlooid <pavlooo987@gmail.com>
2024-01-22 16:58:18 +0300

    Added auth with vkontakte

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'skim'
 gem 'omniauth'
 gem 'omniauth-github'
 gem 'omniauth-rails_csrf_protection'
+gem 'omniauth-vkontakte'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,10 +1,20 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
+    sign_in_with_provider('Github')
+  end
+
+  def vkontakte
+    sign_in_with_provider('Vkontakte')
+  end
+
+  private
+
+  def sign_in_with_provider(provider)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user&.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     else
       redirect_to root_path, alert: 'Something went wrong'
     end
